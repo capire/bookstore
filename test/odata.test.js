cfcf5ece9404f8799841f752d93cb200ea70ca6a
Steffen Waldmann <steffen.waldmann@sap.com>
2026-03-20 11:09:24 +0100

    chore: fix bookshop/bookstore tests

--- test/odata.test.js
+++ test/odata.test.js
@@ -5,7 +5,7 @@ axios.defaults.auth = { username: 'alice', password: 'admin' }
 describe('cap/samples - Bookshop APIs', () => {
 
   it('serves $metadata documents in v4', async () => {
-    const { headers, status, data } = await GET `/browse/$metadata`
+    const { headers, status, data } = await GET `/odata/v4/browse/$metadata`
     expect(status).to.equal(200)
     expect(headers).to.contain({ 'odata-version': '4.0' })
     expect(headers['content-type']).to.match(/application\/xml/)
@@ -15,7 +15,7 @@ describe('cap/samples - Bookshop APIs', () => {
 
   it('serves Books?$expand=currency', async () => {
     const USD = { code: 'USD', name: 'US Dollar', descr: null, symbol: '$' }
-    const { data } = await GET `/browse/Books ${{
+    const { data } = await GET `/odata/v4/browse/Books ${{
       params: { $search: 'Po', $select: `title,author,genre`, $expand:`currency` },
     }}`
     expect(data.value).to.containSubset([
@@ -25,7 +25,7 @@ describe('cap/samples - Bookshop APIs', () => {
   })
 
   it('serves Books?$select=currency/code', async () => {
-    const { data } = await GET `/browse/Books ${{
+    const { data } = await GET `/odata/v4/browse/Books ${{
       params: { $search: 'Po', $select: `title,author,genre,currency/code` },
     }}`
     expect(data.value).to.containSubset([
@@ -37,7 +37,7 @@ describe('cap/samples - Bookshop APIs', () => {
   describe('query options...', () => {
 
     it('supports $search in multiple fields', async () => {
-      const { data } = await GET `/browse/Books ${{
+      const { data } = await GET `/odata/v4/browse/Books ${{
         params: { $search: 'Po', $select: `title,author` },
       }}`
       expect(data.value).to.containSubset([
@@ -49,7 +49,7 @@ describe('cap/samples - Bookshop APIs', () => {
     })
 
     it('supports $select', async () => {
-      const { data } = await GET(`/browse/Books`, {
+      const { data } = await GET(`/odata/v4/browse/Books`, {
         params: { $select: `ID,title` },
       })
       expect(data.value).to.containSubset([
@@ -82,13 +82,13 @@ describe('cap/samples - Bookshop APIs', () => {
     })
 
     it('supports $top/$skip paging', async () => {
-      const { data: p1 } = await GET`/browse/Books?$select=title&$top=3`
+      const { data: p1 } = await GET`/odata/v4/browse/Books?$select=title&$top=3`
       expect(p1.value).to.containSubset([
         { ID: 201, title: 'Wuthering Heights' },
         { ID: 207, title: 'Jane Eyre' },
         { ID: 251, title: 'The Raven' },
       ])
-      const { data: p2 } = await GET`/browse/Books?$select=title&$skip=3`
+      const { data: p2 } = await GET`/odata/v4/browse/Books?$select=title&$skip=3`
       expect(p2.value).to.containSubset([
         { ID: 252, title: 'Eleonora' },
         { ID: 271, title: 'Catweazle' },
